8b682f292d93eab8f75d718e6c7b82f80a623cc8
Sebastian Jankovic <79214003+Whey-ten@users.noreply.github.com>
2023-07-18 16:30:34 +0200

    Add files via upload

---
+++ Modbus-Server/Modbus-Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EasyModbus;
+
+namespace ModbusTCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModbusServer modbusServer = new ModbusServer();
+
+            // Assign static values to the server's holding registers.
+            // Assuming 10 registers here; adjust as needed.
+            for (int i = 0; i < 10; i++)
+            {
+                modbusServer.holdingRegisters[i] = (short)(i * 10);
+            }
+
+            // Handle client connections and data retrieval.
+            modbusServer.HoldingRegistersChanged += (startAddress, quantity) =>
+            {
+                Console.WriteLine($"Client connected. Data was sent from register start address {startAddress} for {quantity} registers.");
+            };
+
+            // Start the server.
+            modbusServer.Listen();
+            Console.WriteLine("Simulated test modbus server was started");
+
+            // Keep the server running until the user decides to quit.
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            // Stop the server.
+            modbusServer.StopListening();
+        }
+    }
+}
