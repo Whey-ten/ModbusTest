ba25a5696bdc64c31fcdf84b7236dc6e342dac5a
Sebastian Jankovic <79214003+Whey-ten@users.noreply.github.com>
2023-07-20 15:02:21 +0200

    Add files via upload

---
+++ Modbus-Server/Modbus-Server-OI/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Timers;
+using EasyModbus;
+
+namespace ModbusTCPServer
+{
+    class Program
+    {
+        static ModbusServer modbusServer;
+        static System.Timers.Timer timer;
+        static Random random;
+
+        static void Main(string[] args)
+        {
+            modbusServer = new ModbusServer();
+            random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                modbusServer.holdingRegisters[i] = GetRandomValue();
+            }
+
+            modbusServer.HoldingRegistersChanged += (startAddress, quantity) =>
+            {
+                Console.WriteLine($"Client connected. Data was sent from register start address {startAddress} for {quantity} registers.");
+            };
+
+            modbusServer.CoilsChanged += (address, coilValue) =>
+            {
+                Console.WriteLine($"Client has written to coil at address {address}. New value: {coilValue}.");
+            };
+
+            modbusServer.Listen();
+            Console.WriteLine("Simulated test Modbus server was started");
+
+            timer = new System.Timers.Timer(30000);
+            timer.Elapsed += TimerElapsed;
+            timer.Start();
+
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            modbusServer.StopListening();
+            timer.Stop();
+        }
+
+        static void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                modbusServer.holdingRegisters[i] = GetRandomValue();
+            }
+        }
+
+        static short GetRandomValue()
+        {
+            return (short)random.Next(10, 91);
+        }
+    }
+}
