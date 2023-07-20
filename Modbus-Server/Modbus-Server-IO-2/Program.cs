eaee76d3028e8b65083a9453c75cc11d493eb661
Sebastian Jankovic <79214003+Whey-ten@users.noreply.github.com>
2023-07-20 15:03:20 +0200

    Add files via upload

---
+++ Modbus-Server/Modbus-Server-IO-2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modbus_Server_IO_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
