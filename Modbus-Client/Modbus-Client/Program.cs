7fac5d5246e1e2f43a63891cbe1154cfd40a1276
Sebastian Jankovic <79214003+Whey-ten@users.noreply.github.com>
2023-07-28 15:28:15 +0200

    Update Program.cs

--- Modbus-Client/Modbus-Client/Program.cs
+++ Modbus-Client/Modbus-Client/Program.cs
@@ -7,24 +7,18 @@ namespace ModbusTCPClient
     {
         static void Main(string[] args)
         {
-            // Create a Modbus TCP client.
             ModbusClient modbusClient = new ModbusClient("127.0.0.1", 502); // Replace with your server's IP address and port.
 
             try
             {
-                // Connect to the server.
                 modbusClient.Connect();
-
-                // Check if the connection was successful.
+                
                 if (modbusClient.Connected)
                 {
                     Console.WriteLine("Successfully connected to the server.");
                 }
-
-                // Read 10 holding registers starting at address 0.
                 int[] values = modbusClient.ReadHoldingRegisters(0, 10);
 
-                // Print the retrieved values.
                 for (int i = 0; i < values.Length; i++)
                 {
                     Console.WriteLine($"Register {i}: {values[i]}");
@@ -36,7 +30,6 @@ namespace ModbusTCPClient
             }
             finally
             {
-                // Always disconnect when finished.
                 if (modbusClient.Connected)
                 {
                     modbusClient.Disconnect();
