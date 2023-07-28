43dfd534b6f46935390094f0426318f61597ac5a
Sebastian Jankovic <79214003+Whey-ten@users.noreply.github.com>
2023-07-28 15:26:57 +0200

    Update Program.cs

--- Modbus-Server/Modbus-Server-Changing/Program.cs
+++ Modbus-Server/Modbus-Server-Changing/Program.cs
@@ -14,41 +14,33 @@ namespace ModbusTCPServer
         {
             modbusServer = new ModbusServer();
             random = new Random();
-
-            // Assign static values to the server's holding registers.
-            // Assuming 10 registers here; adjust as needed.
+            
             for (int i = 0; i < 10; i++)
             {
                 modbusServer.holdingRegisters[i] = GetRandomValue();
             }
-
-            // Handle client connections and data retrieval.
+            
             modbusServer.HoldingRegistersChanged += (startAddress, quantity) =>
             {
                 Console.WriteLine($"Client connected. Data was sent from register start address {startAddress} for {quantity} registers.");
             };
 
-            // Start the server.
             modbusServer.Listen();
             Console.WriteLine("Simulated test Modbus server was started");
-
-            // Start the timer to update register values every 30 seconds.
+            
             timer = new System.Timers.Timer(30000);
             timer.Elapsed += TimerElapsed;
             timer.Start();
 
-            // Keep the server running until the user decides to quit.
             Console.WriteLine("Press any key to stop the server...");
             Console.ReadKey();
 
-            // Stop the server and timer.
             modbusServer.StopListening();
             timer.Stop();
         }
 
         static void TimerElapsed(object sender, ElapsedEventArgs e)
         {
-            // Update the register values every 30 seconds.
             for (int i = 0; i < 10; i++)
             {
                 modbusServer.holdingRegisters[i] = GetRandomValue();
@@ -57,7 +49,7 @@ namespace ModbusTCPServer
 
         static short GetRandomValue()
         {
-            return (short)random.Next(10, 91); // Generates a random value between 10 and 90 (inclusive).
+            return (short)random.Next(10, 91);
         }
     }
 }
